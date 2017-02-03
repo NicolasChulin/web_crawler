7a1dffc70e2439444f5447c58f425002363fcaa9
chulin <chulinice@sina.com>
2017-02-03 15:35:08 +0800

    init files

---
+++ libs/dblink.py
@@ -0,0 +1,2 @@
+
+from web_crawler import setting
---
+++ setting.py
@@ -0,0 +1,9 @@
+
+
+DEFAULT_DB = {
+    'NAME': 'webcrawler',
+    'HOST': 'localhost',
+    'PORT': '3306',
+    'USER': 'root',
+    'PASSWORD': '123456'
+}
